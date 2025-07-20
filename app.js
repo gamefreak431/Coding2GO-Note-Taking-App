8007f6555035b9adb249d504f861a70f21c47774
John McLeish <mcleish.john@gmail.com>
2025-07-20 20:19:09 +1000

    created app.js to apply scripting. Wrote open and close functions for notes

---
+++ app.js
@@ -0,0 +1,14 @@
+let notes = [];
+
+function openNoteDialog() {
+    const dialog = document.getElementById('noteDialog');
+    const titleInput = document.getElementById('noteTitle');
+    const contentInput = document.getElementById('noteContent');
+
+    dialog.showModal();
+    titleInput.focus();
+}
+
+function closeNoteDialog() {
+    document.getElementById('noteDialog').close();
+}
